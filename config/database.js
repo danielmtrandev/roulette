ed939fd6f0b256119c36bd105abef3b4e79cee10
Daniel <danielmtran.dev@gmail.com>
2021-10-25 15:29:16 -0400

    roulette

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://Daniel:SzHInKisNNrW1sfq@cluster0.bbitw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+    'dbName': 'roulette'
+};
